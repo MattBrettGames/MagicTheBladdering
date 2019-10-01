038d2f230258514d52795b2fe3c04a094bffb0a1
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2019-10-01 14:51:36 +0100

    Changed charatcer selector screen

---
+++ FunGame/Assets/Scripts/Brett'sStupidScripts/CubeSpinner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpinner : BlankMono
+{
+    public Vector3 Angle;
+    public float speed;
+    void Update()
+    {
+        gameObject.transform.Rotate(Angle, speed);
+    }
+}
