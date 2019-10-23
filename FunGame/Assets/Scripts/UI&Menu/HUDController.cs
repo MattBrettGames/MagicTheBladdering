2c5af0c331521be4ab23f7b0432363117dd8ccc9
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2019-10-23 15:42:54 +0100

    Made the UI char-images work, maybe

--- FunGame/Assets/Scripts/UI&Menu/HUDController.cs
+++ FunGame/Assets/Scripts/UI&Menu/HUDController.cs
@@ -8,10 +8,10 @@ public class HUDController : BlankMono
     public GameObject healthBar;
     public GameObject barBorder;
     public PlayerBase targetPlayer;
-    public string thisPlayer;
-    public List<Sprite> images = new List<Sprite>();
+    public string thisPlayerInt;
+    public GameObject[] images = new GameObject[2];
     public List<Vector3> imagePos = new List<Vector3>();
-    public Image image;
+    public GameObject image;
     
     public void Start()
     {
@@ -21,10 +21,14 @@ public class HUDController : BlankMono
 
     public void SetStats(int imageInt)
     {
-        targetPlayer = GameObject.FindGameObjectWithTag("Player" + thisPlayer).GetComponent<PlayerBase>();
+        targetPlayer = GameObject.FindGameObjectWithTag("Player" + thisPlayerInt).GetComponent<PlayerBase>();
         healthBar.transform.localScale = new Vector3(targetPlayer.currentHealth / 50f, 0.2f, 1);
         barBorder.transform.localScale = new Vector3(targetPlayer.currentHealth / 50f, 0.2f, 1);
-        image.sprite = images[imageInt];
+
+        image.SetActive(false);
+        image = images[imageInt];
+        image.SetActive(true);
+
         image.transform.position = imagePos[imageInt];
     }
 
