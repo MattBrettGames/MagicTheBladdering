afb400a46f1738c61ea3f6a182cedd2ced64be47
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2019-11-18 08:29:24 +0000

    Added blank Carmen code

---
+++ FunGame/Assets/Scripts/Players/Carmen/Carmen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carmen : PlayerBase
+{
+
+
+
+
+
+    public override void AAction() { }
+    public override void BAction() { }
+    public override void XAction() { }
+    public override void YAction() { }
+
+
+}
