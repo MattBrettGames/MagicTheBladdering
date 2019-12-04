6355ac2eaeb64548a9d92f352e0f35e2e5362e14
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2019-12-04 14:37:50 +0000

    Added manual Character Rotater

---
+++ FunGame/Assets/Scripts/UI&Menu/CharacterRotater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class CharacterRotater : MonoBehaviour
+{
+    Player player;
+    [SerializeField] int thisPInt;
+
+    [SerializeField] float speed;
+    void Start()
+    {
+        player = ReInput.players.GetPlayer(thisPInt);
+    }
+
+    void Update()
+    {
+        transform.Rotate(0, 1 * speed, 0);
+    }
+}
