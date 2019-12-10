8399b37ffba697e5c7900ef5f4fdce894750c63c
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2019-12-10 18:07:44 +0000

    Fixed Rewired Pause stuff

--- FunGame/Assets/Scripts/UI&Menu/PauseMenu.cs
+++ FunGame/Assets/Scripts/UI&Menu/PauseMenu.cs
@@ -2,47 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Rewired;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     Player players;
     Player player2;
-
     [SerializeField] GameObject[] options;
     [Space]
     [SerializeField] string[] optionStrings;
     [Space]
     [SerializeField] GameObject selector;
-    int currentDisplay;
+    int currentDisplay = 0;
     [Space]
     [SerializeField] GameObject visuals;
+    private PlayerBase playerCode1;
+    private PlayerBase playerCode2;
 
     void Start()
     {
         visuals.SetActive(false);
         players = ReInput.players.GetPlayer(0);
         player2 = ReInput.players.GetPlayer(1);
+
+        playerCode1 = GameObject.Find("Player1Base").GetComponentInParent<PlayerBase>();
+        playerCode2 = GameObject.Find("Player2Base").GetComponentInParent<PlayerBase>();
     }
 
     void Update()
     {
-        if (players.GetButtonDown("Pause") || player2.GetButtonDown("Pause"))
+        if (players.GetButtonDown("Pause") | player2.GetButtonDown("Pause"))
         {
-            print("Pause button has been received");
             visuals.SetActive(true);
+            playerCode1.BeginActing();
+            playerCode2.BeginActing();
             Time.timeScale = Mathf.Epsilon;
         }
 
         if (visuals.activeSelf)
         {
-            if (players.GetAxis("VertMove") >= 0.4f|| player2.GetAxis("VertMove") >= 0.4f)
+            print(optionStrings[currentDisplay] +" IS THE CURRENT OPTION" + currentDisplay);
+            if (players.GetAxis("VertMove") >= 0.4f | player2.GetAxis("VertMove") >= 0.4f)
             {
                 if (currentDisplay < options.Length - 1) currentDisplay++;
                 else currentDisplay = 0;
                 selector.transform.localPosition = options[currentDisplay].transform.position;
             }
-            if (players.GetAxis("VertMove") <= 0.4f || player2.GetAxis("VertMove") <= 0.4f)
+            if (players.GetAxis("VertMove") <= -0.4f | player2.GetAxis("VertMove") <= -0.4f)
             {
                 if (currentDisplay != 0) currentDisplay--;
                 else currentDisplay = options.Length - 1;
@@ -59,12 +65,13 @@ public class PauseMenu : MonoBehaviour
     {
         visuals.SetActive(false);
         Time.timeScale = 1;
+        playerCode1.EndActing();
+        playerCode2.EndActing();
     }
 
     void Quit()
     {
-        UniverseController universe = GameObject.Find("UniverseController").GetComponent<UniverseController>();
-        universe.ReturnToMenu();
+        SceneManager.LoadScene("MainMenu");
     }
 
 }
