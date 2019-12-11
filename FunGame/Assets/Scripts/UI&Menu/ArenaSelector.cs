5bdc3b6a85086757338f35a3f31e1e3600462d57
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2019-12-11 20:43:41 +0000

    Total Arena Select overhaul

--- FunGame/Assets/Scripts/UI&Menu/ArenaSelector.cs
+++ FunGame/Assets/Scripts/UI&Menu/ArenaSelector.cs
@@ -1,28 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ArenaSelector : BlankMono
 {
     private bool inputCooldown;
     public List<GameObject> displays;
+    public List<Vector3> camPos;
+    public List<Vector3> camAnglesOffset;
+    [Space]
+    [SerializeField] float targetWidth;
+
+
+    [Header("UI")]
+    public Text arenaName;
+
+
     private int currentDisplay;
+    List<Outline> displayOutlines = new List<Outline>();
+    private UniverseController universe;
+
+    void Start()
+    {
+        for (int i = 0; i < displays.Count; i++)
+        {
+            print(displays[i].GetComponent<Outline>());
+            displayOutlines.Add(displays[i].GetComponent<Outline>());
+            displayOutlines[i].OutlineWidth = 0;
+        }
+        universe = GameObject.FindGameObjectWithTag("UniverseController").GetComponent<UniverseController>();
+    }
+
 
     void Update()
     {
+        Camera.main.transform.LookAt(displays[currentDisplay].transform.position + camAnglesOffset[currentDisplay]);
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, camPos[currentDisplay], 0.16f);
+
         if (Input.GetAxis("AllHorizontal") >= 0.4f && !inputCooldown)
         {
             if (currentDisplay < displays.Count - 1)
             {
-                displays[currentDisplay].SetActive(false);
+                displayOutlines[currentDisplay].enabled = false;
                 currentDisplay++;
-                displays[currentDisplay].SetActive(true);
+                displayOutlines[currentDisplay].enabled = true;
+                arenaName.text = displays[currentDisplay].name;
             }
             else
             {
-                displays[currentDisplay].SetActive(false);
+                displayOutlines[currentDisplay].enabled = false;
                 currentDisplay = 0;
-                displays[currentDisplay].SetActive(true);
+                displayOutlines[currentDisplay].enabled = true;
+                arenaName.text = displays[currentDisplay].name;
             }
             inputCooldown = true;
             Invoke("EndCooldown", 0.3f);
@@ -31,15 +61,17 @@ public class ArenaSelector : BlankMono
         {
             if (currentDisplay != 0)
             {
-                displays[currentDisplay].SetActive(false);
+                displayOutlines[currentDisplay].enabled = false;
                 currentDisplay--;
-                displays[currentDisplay].SetActive(true);
+                displayOutlines[currentDisplay].enabled = true;
+                arenaName.text = displays[currentDisplay].name;
             }
             else
             {
-                displays[currentDisplay].SetActive(false);
+                displayOutlines[currentDisplay].enabled = false;
                 currentDisplay = displays.Count - 1;
-                displays[currentDisplay].SetActive(true);
+                displayOutlines[currentDisplay].enabled = true;
+                arenaName.text = displays[currentDisplay].name;
             }
             inputCooldown = true;
             Invoke("EndCooldown", 0.3f);
@@ -47,14 +79,12 @@ public class ArenaSelector : BlankMono
 
         if (Input.GetButtonDown("AllAButton"))
         {
-            UniverseController universe = GameObject.FindGameObjectWithTag("UniverseController").GetComponent<UniverseController>();
             universe.ChooseArena(displays[currentDisplay].name);
         }
 
         if (Input.GetKeyDown(KeyCode.G))
         {
-            UniverseController universe = GameObject.FindGameObjectWithTag("UniverseController").GetComponent<UniverseController>();
-            universe.ChooseArena("RuinedVillage");
+            universe.ChooseArena(displays[currentDisplay].name);
         }
 
     }
