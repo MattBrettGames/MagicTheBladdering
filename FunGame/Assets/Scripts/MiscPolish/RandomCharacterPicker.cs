92aca16d0205fadc9e69cc1530bcb399b48f02dc
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2020-01-06 09:27:52 +0000

    Added appropriate MainMenu music

--- FunGame/Assets/Scripts/MiscPolish/RandomCharacterPicker.cs
+++ FunGame/Assets/Scripts/MiscPolish/RandomCharacterPicker.cs
@@ -6,15 +6,18 @@ using System;
 public class RandomCharacterPicker : MonoBehaviour
 {
     public MenuSelector menu;
+    AudioManager audioManager;
     [Space]
     public GameObjects[] chars = new GameObjects[2];
-
+    public String[] themes = new string[0];
 
     void Start()
     {
         int rando = UnityEngine.Random.Range(0, chars.Length);
         chars[rando].characters.SetActive(true);
         menu.cursor = menu.cursors[rando];
+        audioManager = GameObject.FindGameObjectWithTag("UniverseController").GetComponentInChildren<AudioManager>();
+        audioManager.Play(themes[rando]);
     }
 
     [Serializable]
