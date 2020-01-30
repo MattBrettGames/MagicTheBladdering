b92377ee7d864413fceacbca49f16a24f05b6a51
zionlove99 <55879222+zionlove99@users.noreply.github.com>
2020-01-30 17:52:57 +0000

    More Demon caves stuff

---
+++ FunGame/Assets/GameAssets/EnvironmentAssets/DemonCaves/ZionsShit/Scripts/FireJet.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireJet : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem particleSystem;
+
+    private GameObject collisionBox;
+    private Animator anim;
+
+    public FireJetHandler fjh;
+
+    [SerializeField]
+    private GameObject lightSource;
+
+    //[System.NonSerialized]
+    public  bool isActive;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        isActive = false;
+    }
+    public void setActive(float Duration)
+    {
+        isActive = true;
+        StartCoroutine(fire(Duration));
+    }
+
+    IEnumerator fire(float duration)
+    {
+        Debug.Log("fire");
+        //allow light and emission to fade in;
+        //make emission fade in first;
+        anim.SetBool("FadeIn", true);
+
+        lightSource.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+
+        anim.SetBool("FadeIn", false);
+        //activate all visuals etc.
+        collisionBox.SetActive(true);
+        particleSystem.Play();
+        yield return new WaitForSeconds(duration - 1);
+        particleSystem.Stop();
+        collisionBox.SetActive(false);
+        anim.SetBool("FadeOut", true);
+        yield return new WaitForSeconds(1f);
+        anim.SetBool("FadeOut", false);
+
+        Debug.Log("stopFire");
+
+        isActive = false;
+        //set emission to fade
+
+        lightSource.SetActive(false);
+
+        fjh.SetInactive();
+        
+
+        //StopCoroutine(fire(0));
+    }
+
+}
---
+++ FunGame/Assets/GameAssets/EnvironmentAssets/DemonCaves/ZionsShit/Scripts/FireJetHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireJetHandler : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> JetObj = new List<GameObject>(); //create class for these
+
+    [SerializeField]
+    private float maxTime = 15f;
+    [SerializeField]  //time between outbursts
+    private float minTime = 8f;
+    [SerializeField]
+    private float maxDuration = 5f;
+    [SerializeField]     //random between these two values. length of outburst
+    private float minDuration = 3f;
+
+    [SerializeField]
+    private int maxActive = 3;   //max no active at once.
+
+    public int currentActive = 0;
+
+    void Start()
+    {
+        StartCoroutine(outburstTimer()); //get going.
+    }
+
+
+    public void SetInactive()
+    {
+        currentActive -= 1;
+    }
+
+    IEnumerator outburstTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minTime, maxTime));
+           
+            foreach(GameObject go in JetObj)
+            {
+                FireJet fj = go.GetComponent<FireJet>();
+                if (!fj.isActive && currentActive < maxActive)
+                {
+                    fj.setActive(Random.Range(minDuration, maxDuration));
+                    currentActive += 1;
+                    break;
+                }
+                yield return null;
+            }
+        }
+    }
+
+
+}
