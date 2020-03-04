56956c44e9aef50f8405aaab7076811923aab4e1
Nick-Furniss <44497325+Nick-Furniss@users.noreply.github.com>
2020-03-04 14:21:56 +0000

    Sound should play on start, and not override

--- FunGame/Assets/Scripts/MiscPolish/RandomCharacterPicker.cs
+++ FunGame/Assets/Scripts/MiscPolish/RandomCharacterPicker.cs
@@ -10,15 +10,24 @@ public class RandomCharacterPicker : MonoBehaviour
     [Space]
     public GameObjects[] chars = new GameObjects[2];
     public AudioClip[] themes = new AudioClip[0];
+    AudioSource thisSource;
 
     void Start()
     {
         int rando = UnityEngine.Random.Range(0, chars.Length);
         chars[rando].characters.SetActive(true);
         menu.cursor = menu.cursors[rando];
+        thisSource = gameObject.AddComponent<AudioSource>();
 
-        audioManager.volume = OptionMenuController.masterVolume * OptionMenuController.musicVolume;
-        audioManager.PlayOneShot(themes[rando]);
+        if (thisSource.clip != themes[rando] || thisSource.clip == null)
+        {
+            thisSource.Stop();
+            thisSource.clip = themes[rando];
+            thisSource.Play();
+        }
+
+        //audioManager.volume = OptionMenuController.masterVolume * OptionMenuController.musicVolume;
+        //audioManager.PlayOneShot(themes[rando]);
     }
 
     [Serializable]
